4aff1aed8244e460b2119ed61006fdc751f7460c
Luka <lukhanyo95@gmail.com>
2021-07-04 04:49:58 +0200

    added a contact form

--- contactlukhanyo.php
+++ contactlukhanyo.php
@@ -1,14 +1,14 @@
-<link href="https://fonts.googleapis.com/css2?family=Aclonica&display=swap" rel="stylesheet"> 
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="contactlukhanyo.css">
-        <title>Contact Lukhanyo</title>
+<link href="https://fonts.googleapis.com/css2?family=Aclonica&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="contactlukhanyo.css">
+<title>Contact Lukhanyo</title>
 
-        <?php
+<?php
             include_once "includes/head.php";
         ?>
-    
-    <body>
+
+<body>
     <div class="wrapper">
         <header>
             <nav>
@@ -19,7 +19,7 @@
                 </div>
 
                 <ul class="nav_list">
-                    <li ><a href="index.php">Home</a></li>
+                    <li><a href="index.php">Home</a></li>
                     <li><a href="aboutlukhanyo.php">About</a></li>
                     <li><a href="contactlukhanyo.php" class="active">Contact</a></li>
                 </ul>
@@ -27,23 +27,15 @@
         </header>
 
         <main>
-            <h2>Contact Lukhanyo</h2>
+            <h2> Get in Touch</h2>
 
-            <form action="contactlukhanyo.php">
-
-
+            <form action="getintouch.php" method="post">
+                <input type="text" name="name" placeholder="Full name/ Company">
+                <input type="email" name="email" placeholder="Email">
+                <input type="text" name="subject" placeholder="Subject">
+                <textarea name="message" placeholder="Enter messsage"></textarea>
+                <input type="submit" name="submit" value="SEND">
             </form>
-
-            <article>
-               <div class="phone">
-                    <i class="fa fa-phone" aria-hidden="true"></i>
-                    <p>067 6473 714</p>
-               </div>
-                <div class="email">
-                    <i class="fa fa-envelope" aria-hidden="true"></i>
-                    <p><a class="email_style" href="mailto:lukhanyo95@gmail.com">lukhanyo95@gmail.com</a></p>
-                </div>
-            </article>   
         </main>
 
         <?php
---
+++ getintouch.php
@@ -0,0 +1,20 @@
+<?php
+
+// run only when the submit button is clicked
+
+if(isset($_POST['submit'])){
+    // Use trim to remove the white spaces in input fields
+    $userName = trim($_POST["name"]);
+    $userEmail = trim($_POST["email"]);
+    $subject = trim($_POST["subject"]);
+    $userMessage = trim($_POST["message"]);
+
+    $myEmail = "lukhanyo@fundza.co.za";
+    $emailFrom = "From: " . $email;
+    $message = "You have received an email from ". $userName. ". \n\n". $userMessage; 
+
+    mail($myEmail, $subject, $userMessage, $emailFrom);
+    header("Location: contactlukhanyo.php?sentSuccesfully");
+}
+
+?>
